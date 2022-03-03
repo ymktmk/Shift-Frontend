c1c47dc5a59e987fda75f600cc4a1a943cd23ab6
Azuki Yamada <yamada.azuki@aoaoi.work>
2022-03-03 14:39:12 +0900

    Update login.js

--- pages/login.js
+++ pages/login.js
@@ -4,6 +4,7 @@ import tw from "tailwind-styled-components"
 const Login = () => {
   return (
     <Wrapper>
+        <AppImage alt="仮の画像だよ" src="https://1.bp.blogspot.com/-HkohqHrBbJI/VN3vDch8otI/AAAAAAAAris/0NgCSPANHss/s800/hinamatsuri_odairi_ohina.png"/>
         <LoginContainer>
             <InputEmailAddress>
 
@@ -28,5 +29,23 @@ const Login = () => {
 export default Login
 
 const Wrapper = tw.div`
+    h-screen
+    flex
+    flex-col
+`
+
+const AppImage = tw.img``
+
+const LoginContainer = tw.div`
 
 `
+
+const InputEmailAddress = tw.input``
+
+const InputPassword = tw.input``
+
+const LoginButton = tw.button``
+
+const Border = tw.div``
+
+const GoogleLoginButton = tw.button``
