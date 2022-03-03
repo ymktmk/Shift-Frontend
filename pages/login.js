c515e88bc24c437ceb052835f84229a7dc7143cd
Azuki Yamada <yamada.azuki@aoaoi.work>
2022-03-03 14:13:14 +0900

    [create] login.js

---
+++ pages/login.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import tw from "tailwind-styled-components"
+
+const Login = () => {
+  return (
+    <Wrapper>
+        <LoginContainer>
+            <InputEmailAddress>
+
+            </InputEmailAddress>
+            <InputPassword>
+
+            </InputPassword>
+            <LoginButton>
+
+            </LoginButton>
+        </LoginContainer>
+        <Border>
+
+        </Border>
+        <GoogleLoginButton>
+
+        </GoogleLoginButton>
+    </Wrapper>
+  )
+}
+
+export default Login
+
+const Wrapper = tw.div`
+
+`
